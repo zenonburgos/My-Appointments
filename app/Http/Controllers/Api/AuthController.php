560235e978120be6e82642eaa38948ff71aeb310
Zenón Burgos <znburgos@gmail.com>
2021-08-27 09:14:41 -0600

    LaraJWT implementation

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+use JwtAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+    	$credentials = $request->only('email', 'password');
+    	//return 'Público';
+
+    	if (Auth::guard('api')->attempt($credentials)) {
+		    $user = Auth::guard('api')->user();
+
+		    $jwt = JwtAuth::generateToken($user);
+		    $error = false;
+		    //$success = true;
+		    
+		    // Return successfull sign in response with the generated jwt.
+		    //return compact('success', 'user', 'jwt');
+		    $data = compact('user', 'jwt');
+		    return compact('error', 'data');
+		} else {
+		    // Return response for failed attempt.
+			/*$success = false;
+			$message = 'Invalid credentials';
+			return compact('success', 'message');*/
+
+			$error = true;
+			$message = 'Invalid credentials';
+			return compact('error', 'message');
+		}
+    }
+}
